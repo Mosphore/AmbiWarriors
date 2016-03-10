628e7f65bb58805cfbbc29256722756a4eb1651d
Mosphore <dorianessamir.w@gmail.com>
2016-03-10 10:01:25 +0100

    1st commit

---
+++ Test_SteamController/Assets/Scripts/joysticktest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class joysticktest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0, 0);
+	}
+}
